bf5cbb82f743e4169feca184d52c0e59279693bb
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-15 17:52:26 -0300

    feat: Create user validating username

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    request.user = user;
+
+    return response.status(400).json({ error: 'User already exists.' });
+  }
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  let user = users.find(user => user.username === username);
+
+  if (user) {
+    return response.status(400).json({ error: 'User already exists.' });
+  }
+
+  user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
